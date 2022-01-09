448d1db7b87cb5e92c7a4f38a9f426f467a5e736
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-01-10 01:00:40 +0300

    Update VuetifyThemeCore.php

--- themes/vuetifyCore/VuetifyThemeCore.php
+++ themes/vuetifyCore/VuetifyThemeCore.php
@@ -3,8 +3,19 @@ namespace themes\vuetifyCore;
 
 use webfiori\framework\Theme;
 use webfiori\ui\HTMLNode;
-
+/**
+ * A base theme for creating Vuetify based themes.
+ * 
+ * This theme is used to re-structure a web page based on common Vue and Vuetify
+ * page structure. It simply create a top element which acts as the root 'application'
+ * element. Inside the element placed other page sections.
+ */
 abstract class VuetifyThemeCore extends Theme {
+    /**
+     * 
+     * @var string The ID of the element that represents the 'application' element
+     */
+    private $appElId;
     /**
      * Creates new instance of the class.
      * 
@@ -12,7 +23,7 @@ abstract class VuetifyThemeCore extends Theme {
      */
     public function __construct($themeName = '') {
         parent::__construct($themeName);
-        $this->setVersion('1.2');
+        $this->setVersion('1.2.5');
         $this->setAuthor('Ibrahim BinAlshikh');
         $this->setDescription('This theme creates basic page structure for '
                 .'building themes which can be based on Vuetify. Vuetify is a '
@@ -21,12 +32,13 @@ abstract class VuetifyThemeCore extends Theme {
         $this->setAuthorUrl('https://ibrahim-binalshikh.me');
         $this->setLicenseName('MIT');
         $this->setLicenseUrl('https://opensource.org/licenses/MIT');
-
-        $this->setAfterLoaded(function (Theme $theme)
+        $this->setAppID('app');
+        
+        $this->setAfterLoaded(function (VuetifyThemeCore $theme)
         {
             $page = $theme->getPage();
             $vueEl = new HTMLNode('div', [
-                'id' => 'app'
+                'id' => $theme->getAppID()
             ]);
             $appEl = $vueEl->addChild('v-app');
             $appEl->addChild($page->removeChild('page-header'));
@@ -39,5 +51,25 @@ abstract class VuetifyThemeCore extends Theme {
                 'src' => 'https://cdn.jsdelivr.net/gh/webfiori/vuetifyCore@1.x.x/themes/vuetifyCore/default.js'
             ]);
         });
+    }
+    /**
+     * Returns the ID of the 'div' that will be used as root Vue application.
+     *  
+     * @return string Default return value is 'app'.
+     */
+    public function getAppID() {
+        return $this->appElId;
+    }
+    /**
+     * Sets the ID of the 'div' that will be used as root Vue application.
+     * 
+     * @param string $id A non empty string.
+     */
+    public function setAppID($id) {
+        $trimmed = trim($id);
+        
+        if (strlen($trimmed) > 0) {
+            $this->appElId = $trimmed;
+        }
     }
 }
