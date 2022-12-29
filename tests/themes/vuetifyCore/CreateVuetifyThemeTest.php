695c2fab357945304886d7f38ddac3610f1309b6
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2022-12-29 18:09:50 +0300

    Create CreateVuetifyThemeTest.php

---
+++ tests/themes/vuetifyCore/CreateVuetifyThemeTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace webfiori\framework\test\cli;
+
+use PHPUnit\Framework\TestCase;
+
+class CreateVuetifyThemeTest extends TestCase {
+    /**
+     * @test
+     */
+    public function test00() {
+        $this->assertTrue(true);
+    }
+}
