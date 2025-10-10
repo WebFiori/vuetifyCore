ff68a0263dc161ff2f6134aac15211724df5c9a0
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2025-10-10 20:38:22 +0300

    Update CreateVuetifyThemeTest.php

--- tests/Themes/VuetifyCore/CreateVuetifyThemeTest.php
+++ tests/Themes/VuetifyCore/CreateVuetifyThemeTest.php
@@ -1,13 +1,39 @@
 <?php
 namespace webfiori\framework\test\cli;
 
-use PHPUnit\Framework\TestCase;
+use Themes\VuetifyCore\Cli\CreateVuetifyThemeCommand;
+use WebFiori\Cli\CommandTestCase;
 
-class CreateVuetifyThemeTest extends TestCase {
+class CreateVuetifyThemeTest extends CommandTestCase {
     /**
      * @test
      */
     public function test00() {
-        $this->assertTrue(true);
+        $ns = 'TestTheme\\My';
+        $name = 'MyCoolVuetifyTheme';
+        $path = ROOT_PATH.DS.$ns;
+        $output = $this->executeSingleCommand(new CreateVuetifyThemeCommand(), [], [
+            '0',
+            $name,
+            $ns
+        ]);
+        $this->assertEquals([
+            "Select theme wireframe:\n",
+            "0: Base <--\n",
+            "1: Extended Toolbar\n",
+            "2: System Bar\n",
+            "3: Inbox\n",
+            "4: Side Navigation\n",
+            "Enter a name for the new class:\n",
+            "Enter namespace for the class: Enter = 'Themes\Vuetify'\n",
+            "Creating new vuetify theme based on 'Base' wireframe...\n",
+            "Your theme was successfully created.\n"
+        ], $output);
+        $this->assertTrue(file_exists($path.DS.$name.'.php'), "File not found: ".$path.DS.$name.'.php');
+        $this->assertTrue(class_exists($ns.'\\'.$name), "Class not found: ".$ns.'\\'.$name);
+        $this->assertTrue(class_exists($ns.'\\AsideSection', "Class not found: ".$ns.'\\AsideSection'));
+        $this->assertTrue(class_exists($ns.'\\FooterSection', "Class not found: ".$ns.'\\FooterSection'));
+        $this->assertTrue(class_exists($ns.'\\HeaderSection', "Class not found: ".$ns.'\\HeaderSection'));
+        $this->assertTrue(class_exists($ns.'\\HeadSection', "Class not found: ".$ns.'\\HeadSection'));
     }
 }
