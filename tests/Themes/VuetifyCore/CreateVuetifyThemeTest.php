b2b2b291264587bebed06c97f411da7546a2433b
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2025-10-10 21:04:56 +0300

    Update CreateVuetifyThemeTest.php

--- tests/Themes/VuetifyCore/CreateVuetifyThemeTest.php
+++ tests/Themes/VuetifyCore/CreateVuetifyThemeTest.php
@@ -55,6 +55,118 @@ class CreateVuetifyThemeTest extends CommandTestCase {
     }
     
     /**
+     * @test
+     */
+    public function testExtendedToolbar() {
+        $ns = 'TestTheme\\ExtendedToolbar';
+        $name = 'ExtendedToolbarTheme';
+        $path = ROOT_PATH . DS . str_replace('\\', DS, $ns);
+        
+        $this->cleanupTestResources($path);
+        
+        $output = $this->executeSingleCommand(new CreateVuetifyThemeCommand(), [], [
+            '1',
+            $name,
+            $ns
+        ]);
+        
+        $expectedFiles = [$name . '.php', 'AsideSection.php', 'FooterSection.php', 'HeaderSection.php', 'HeadSection.php'];
+        
+        foreach ($expectedFiles as $fileName) {
+            $filePath = $path . DS . $fileName;
+            $this->assertTrue(file_exists($filePath), "File not found: " . $filePath);
+            require_once $filePath;
+            $this->assertTrue(class_exists($ns.'\\'.explode('.', $fileName)[0]), "Class not found: " . $ns.'\\'.explode('.', $fileName)[0]);
+        }
+        
+        $this->cleanupTestResources($path);
+    }
+    
+    /**
+     * @test
+     */
+    public function testSystemBar() {
+        $ns = 'TestTheme\\SystemBar';
+        $name = 'SystemBarTheme';
+        $path = ROOT_PATH . DS . str_replace('\\', DS, $ns);
+        
+        $this->cleanupTestResources($path);
+        
+        $output = $this->executeSingleCommand(new CreateVuetifyThemeCommand(), [], [
+            '2',
+            $name,
+            $ns
+        ]);
+        
+        $expectedFiles = [$name . '.php', 'AsideSection.php', 'FooterSection.php', 'HeaderSection.php', 'HeadSection.php'];
+        
+        foreach ($expectedFiles as $fileName) {
+            $filePath = $path . DS . $fileName;
+            $this->assertTrue(file_exists($filePath), "File not found: " . $filePath);
+            require_once $filePath;
+            $this->assertTrue(class_exists($ns.'\\'.explode('.', $fileName)[0]), "Class not found: " . $ns.'\\'.explode('.', $fileName)[0]);
+        }
+        
+        $this->cleanupTestResources($path);
+    }
+    
+    /**
+     * @test
+     */
+    public function testInbox() {
+        $ns = 'TestTheme\\Inbox';
+        $name = 'InboxTheme';
+        $path = ROOT_PATH . DS . str_replace('\\', DS, $ns);
+        
+        $this->cleanupTestResources($path);
+        
+        $output = $this->executeSingleCommand(new CreateVuetifyThemeCommand(), [], [
+            '3',
+            $name,
+            $ns
+        ]);
+        
+        $expectedFiles = [$name . '.php', 'AsideSection.php', 'FooterSection.php', 'HeaderSection.php', 'HeadSection.php'];
+        
+        foreach ($expectedFiles as $fileName) {
+            $filePath = $path . DS . $fileName;
+            $this->assertTrue(file_exists($filePath), "File not found: " . $filePath);
+            require_once $filePath;
+            $this->assertTrue(class_exists($ns.'\\'.explode('.', $fileName)[0]), "Class not found: " . $ns.'\\'.explode('.', $fileName)[0]);
+        }
+        
+        $this->cleanupTestResources($path);
+    }
+    
+    /**
+     * @test
+     */
+    public function testSideNavigation() {
+        $ns = 'TestTheme\\SideNavigation';
+        $name = 'SideNavigationTheme';
+        $path = ROOT_PATH . DS . str_replace('\\', DS, $ns);
+        
+        $this->cleanupTestResources($path);
+        
+        $output = $this->executeSingleCommand(new CreateVuetifyThemeCommand(), [], [
+            '4',
+            $name,
+            $ns
+        ]);
+        
+        $expectedFiles = [$name . '.php', 'AsideSection.php', 'FooterSection.php', 'HeaderSection.php', 'HeadSection.php'];
+        
+        foreach ($expectedFiles as $fileName) {
+            $filePath = $path . DS . $fileName;
+            $this->assertTrue(file_exists($filePath), "File not found: " . $filePath);
+            require_once $filePath;
+            $this->assertTrue(class_exists($ns.'\\'.explode('.', $fileName)[0]), "Class not found: " . $ns.'\\'.explode('.', $fileName)[0]);
+        }
+        
+        $this->cleanupTestResources($path);
+    }
+    
+    /**
      * Deletes all resources created by a test in the specified path
      * 
      * @param string $path The path to clean up
